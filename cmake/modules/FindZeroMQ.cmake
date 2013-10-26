dfa93b2d5e6da3671fbb5a4a5c4a713bafaf51b8
Daniel Kirchner <daniel.c.kirchner@googlemail.com>
2013-10-26 05:09:56 +0200

    .

--- cmake/modules/FindZeroMQ.cmake
+++ cmake/modules/FindZeroMQ.cmake
@@ -18,28 +18,70 @@
 #  ZeroMQ_LIBRARIES          The ZeroMQ libraries
 #  ZeroMQ_INCLUDE_DIR        The location of ZeroMQ headers
 
-find_path(ZeroMQ_ROOT_DIR
-    NAMES include/zmq.hpp
-)
+#find_path(ZeroMQ_ROOT_DIR
+#    NAMES include/zmq.hpp
+#)
+#
+#find_library(ZeroMQ_LIBRARIES
+#    NAMES zmq
+#    HINTS ${ZeroMQ_ROOT_DIR}/lib
+#)
+#
+#find_path(ZeroMQ_INCLUDE_DIR
+#    NAMES zmq.hpp
+#    HINTS ${ZeroMQ_ROOT_DIR}/include
+#)
+#
+#include(FindPackageHandleStandardArgs)
+#find_package_handle_standard_args(ZeroMQ DEFAULT_MSG
+#    ZeroMQ_LIBRARIES
+#    ZeroMQ_INCLUDE_DIR
+#)
+#
+#mark_as_advanced(
+#    ZeroMQ_ROOT_DIR
+#    ZeroMQ_LIBRARIES
+#    ZeroMQ_INCLUDE_DIR
+#)
 
-find_library(ZeroMQ_LIBRARIES
-    NAMES zmq
-    HINTS ${ZeroMQ_ROOT_DIR}/lib
-)
+# - Try to find libzmq
+# Once done, this will define
+#
+# ZeroMQ_FOUND - system has libzmq
+# ZeroMQ_INCLUDE_DIRS - the libzmq include directories
+# ZeroMQ_LIBRARIES - link these to use libzmq
 
-find_path(ZeroMQ_INCLUDE_DIR
-    NAMES zmq.hpp
-    HINTS ${ZeroMQ_ROOT_DIR}/include
-)
+include(LibFindMacros)
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(ZeroMQ DEFAULT_MSG
-    ZeroMQ_LIBRARIES
-    ZeroMQ_INCLUDE_DIR
-)
+IF (UNIX)
+        # Use pkg-config to get hints about paths
+        libfind_pkg_check_modules(ZeroMQ_PKGCONF libzmq)
 
-mark_as_advanced(
-    ZeroMQ_ROOT_DIR
-    ZeroMQ_LIBRARIES
-    ZeroMQ_INCLUDE_DIR
-)
+        # Include dir
+        find_path(ZeroMQ_INCLUDE_DIR
+         NAMES zmq.hpp
+         PATHS ${ZEROMQ_ROOT}/include ${ZeroMQ_PKGCONF_INCLUDE_DIRS}
+        )
+
+        # Finally the library itself
+        find_library(ZeroMQ_LIBRARY
+         NAMES zmq
+         PATHS ${ZEROMQ_ROOT}/lib ${ZeroMQ_PKGCONF_LIBRARY_DIRS}
+        )
+ELSEIF (WIN32)
+        find_path(ZeroMQ_INCLUDE_DIR
+         NAMES zmq.hpp
+         PATHS ${ZEROMQ_ROOT}/include ${CMAKE_INCLUDE_PATH}
+        )
+        # Finally the library itself
+        find_library(ZeroMQ_LIBRARY
+         NAMES libzmq
+         PATHS ${ZEROMQ_ROOT}/lib ${CMAKE_LIB_PATH}
+        )
+ENDIF()
+
+# Set the include dir variables and the libraries and let libfind_process do the rest.
+# NOTE: Singular variables for this library, plural for libraries this this lib depends on.
+set(ZeroMQ_PROCESS_INCLUDES ZeroMQ_INCLUDE_DIR ZeroMQ_INCLUDE_DIRS)
+set(ZeroMQ_PROCESS_LIBS ZeroMQ_LIBRARY ZeroMQ_LIBRARIES)
+libfind_process(ZeroMQ)
