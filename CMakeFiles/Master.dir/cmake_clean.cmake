f9d727580876cf6fc1ae91440c57057eeb16856a
Daniel Kirchner <daniel.c.kirchner@googlemail.com>
2013-10-26 02:30:39 +0200

    setting up build system

---
+++ CMakeFiles/Master.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Master.dir/master.cpp.o"
+  "Master.pdb"
+  "Master"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Master.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
