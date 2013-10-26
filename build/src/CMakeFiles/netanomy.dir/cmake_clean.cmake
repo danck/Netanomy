9a42ca2e289b75ed697fab32e386dbc458aa674a
Daniel Kirchner <daniel.c.kirchner@googlemail.com>
2013-10-26 19:28:13 +0200

    initial slave branch commit

---
+++ build/src/CMakeFiles/netanomy.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/netanomy.dir/master.cpp.o"
+  "netanomy.pdb"
+  "netanomy"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/netanomy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
